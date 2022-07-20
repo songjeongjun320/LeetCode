38158b07456eb2c5b7a298fdb61e57802205eb6c
songjeongjun320 <93186548+songjeongjun320@users.noreply.github.com>
2022-07-20 12:37:56 -0700

    Time: 6079 ms (9.03%), Space: 14.2 MB (68.01%) - LeetHub

---
+++ 1-two-sum/1-two-sum.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        switch = True
+        output = []
+        sum = 0
+
+        for i in range(len(nums)):
+            if switch == False:
+                break
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+                else:
+                    if nums[i] + nums[j] == target:
+                        output.append(i)
+                        output.append(j)
+                        switch = False
+                        break
+
+        return output
+        
