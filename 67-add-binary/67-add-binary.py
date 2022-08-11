446f55e81af1499066cbefdd49c84b6eb74199b3
songjeongjun320 <93186548+songjeongjun320@users.noreply.github.com>
2022-08-11 09:21:53 -0700

    Time: 63 ms (5.13%), Space: 13.8 MB (5.89%) - LeetHub

---
+++ 67-add-binary/67-add-binary.py
@@ -0,0 +1,30 @@
+class Solution():
+    def addBinary(self, a, b):
+        a = list(a)
+        b = list(b)
+        a.reverse()
+        b.reverse()
+        output = []
+        c = 0
+        n = max(len(a), len(b))
+        for _ in range(n):
+            sum = 0
+            if a:
+                sum += int(a[0])
+                a.pop(0)
+            if b:
+                sum += int(b[0])
+                b.pop(0)
+            sum += c
+            if sum >= 2:
+                c = 1
+                sum -= 2
+                output.append(str(sum))
+            else:
+                output.append(str(sum))
+                c = 0
+        if c == 1:
+            output.append(str(1))
+        output.reverse()
+        output = ''.join(output)
+        return output
