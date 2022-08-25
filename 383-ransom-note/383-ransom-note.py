e4fa6e9669d0e4d84ff14054026dcae0d8b38067
songjeongjun320 <93186548+songjeongjun320@users.noreply.github.com>
2022-08-25 09:32:27 -0700

    Time: 3248 ms (5.00%), Space: 14.4 MB (6.00%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canConstruct(self, ransomNote, magazine):
+        ransomList = []
+        magazineList = []
+        for _ in range(len(ransomNote)):
+            ransomList.append(ransomNote[_])
+        for _ in range(len(magazine)):
+            magazineList.append(magazine[_])
+        ransomList.sort()
+        magazineList.sort()
+        
+        while ransomList:
+            if ransomList[0] in magazineList:
+                magazineList.remove(ransomList[0])
+                ransomList.pop(0)
+            else:
+                return False
+        return True
