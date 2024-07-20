5c14f2905c0412e080f80562429174cb7c5d76fc
Jun <songjeongjun320@gmail.com>
2024-07-20 15:41:01 -0700

    07/20/2024

---
+++ 151-Reverse words in a string/main.py
@@ -0,0 +1,21 @@
+from typing import List, Tuple
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        output: List[str] = s.split()
+        l: int = 0
+        r: int = len(output) - 1
+        def reverseWords_helper(l, r):
+            if l > r:
+                return
+            else:
+                tmp: str = output[l]
+                output[l] = output[r]
+                output[r] = tmp
+                return reverseWords_helper(l + 1, r - 1)
+        reverseWords_helper(l, r)
+        return output
+
+solution = Solution()
+s = "the sky is blue"
+print(solution.reverseWords(s))
