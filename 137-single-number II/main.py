82fb6fa37f3d5920c10ee69e17f1c6ae0f71a6a5
Jun <songjeongjun320@gmail.com>
2024-07-23 23:50:56 -0700

    07/23/2024

--- 137-single-number II/main.py
+++ 137-single-number II/main.py
@@ -3,6 +3,8 @@ from typing import List, Tuple
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         nums.sort()
+        if len(nums) == 1:
+            return nums[0]
         if nums[0] != nums[1]:
             return nums[0]
         if nums[-1] != nums[-2]:
@@ -11,7 +13,6 @@ class Solution:
             if nums[i-1] != nums[i] != nums[i+1]:
                 return nums[i]
         return None
-
 sol = Solution()
 nums = [0,1,0,1,0,1,99]
 print(sol.singleNumber(nums))
