f56a1de9f3e3fd1bc10262ef63ccd7fc71647955
Jun <songjeongjun320@gmail.com>
2024-07-25 23:32:06 -0700

    07/25/2024

--- 443-String-Compression/main.py
+++ 443-String-Compression/main.py
@@ -2,19 +2,39 @@ from typing import List, Tuple
 
 class Solution:
     def compress(self, chars: List[str]) -> int:
+        if len(chars) == 1:
+            return 1
         prev: int = 0
         curr: int = 0
         cnt: int = 1
         write: int = 0
+        output: str = ""
 
-        while curr < len(chars):
+        while curr < len(chars) - 1:
             if prev == curr:
+                output += chars[prev]
                 curr += 1
-                            
+            if chars[prev] == chars[curr]:
+                cnt += 1
+            else:
+                if cnt != 1:
+                    output += str(cnt)
+                    cnt = 1
+                prev = curr 
+                continue
+            prev += 1
+            curr += 1
+        if chars[prev] == chars[curr]:
+            cnt += 1
+            output += str(cnt)
+        else:
+            output += str(cnt)
 
-        return write
+        for i in range(len(output)):
+            chars[i] = output[i]
+        return chars
 
 
-chars = ["a","a","b","b","c","c","c"]
+chars = ["a","a","a","b","b","a","a"]
 sol = Solution()
 print(sol.compress(chars))
