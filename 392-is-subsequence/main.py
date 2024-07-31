dcf1fb8fb082e2c30534b893c1c3b33076aa0675
Jun <songjeongjun320@gmail.com>
2024-07-30 22:12:36 -0700

    07/30/2024

--- 392-is-subsequence/main.py
+++ 392-is-subsequence/main.py
@@ -2,8 +2,14 @@ from typing import List, Tuple
 
 class Solution:
     def isSubsequence(self, s:str, t:str) -> bool:
-
-        pass
+        l: int = 0
+        for i in range(len(s)):
+            tmp: int = t.find(s[i],l,len(t))
+            if tmp == -1:
+                return False
+            l = tmp + 1
+            
+        return True
 
 sol = Solution()
 s = "abc"
