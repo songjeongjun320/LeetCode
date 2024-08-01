5d1c5ade8f9a767b4e8a272cbaba5ebabc4e7b46
Jun <songjeongjun320@gmail.com>
2024-07-31 22:42:37 -0700

    07/31/2024

---
+++ 1679-Max Number of K-Sum Pairs/main.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        list: List[int] = nums.copy()
+        # list.sort()
+        l: int = 0
+        result: int = 0
+
+        while l <= len(list) - 2:
+            tmp: int = 0
+            try:
+                tmp = list.index(k - list[l], l + 1)
+                list.pop(tmp)
+                list.pop(l)
+                result += 1
+            except:
+                l += 1      
+
+        return result
+    
+    
+
+sol = Solution()
+nums: int = [3,1,3,4,3]
+k: int = 6
+print(sol.maxOperations(nums,k))
