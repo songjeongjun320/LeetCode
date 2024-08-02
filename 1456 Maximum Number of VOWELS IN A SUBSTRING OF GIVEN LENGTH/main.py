d864644d21d1277a098209f0fcfaf30fcbe4ab91
Jun <songjeongjun320@gmail.com>
2024-08-02 12:39:38 -0700

    08/02/2024

---
+++ 1456 Maximum Number of VOWELS IN A SUBSTRING OF GIVEN LENGTH/main.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple
+
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        tmp: list[str] = list(s)
+        pass
+
+
+sol = Solution()
+s: str = "abciiidef"
+k: int = 3
+print(sol. maxVowels(s,k))
+
+print(list(s))
