aa650be2b9098a2f85bced5d64446989ad192ea3
Jun <songjeongjun320@gmail.com>
2024-08-06 11:24:46 -0700

    08/06/2024

---
+++ 1732/main.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        max_height: int = 0
+        curr: int = 0
+        for i in gain:
+            curr += i
+            if curr > max_height:
+                max_height = curr
+        return max_height
+
