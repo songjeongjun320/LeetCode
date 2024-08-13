2129d6e2aedb295175a6e217dd364340a756149f
Jun <songjeongjun320@gmail.com>
2024-08-12 17:21:57 -0700

    08/12/2024

---
+++ 735/main.py
@@ -0,0 +1,10 @@
+from typing import List, Tuple
+
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        return
+    
+
+sol: Solution = Solution()
+asteroids: List[int] = [5,10,-5]
+print(sol.asteroidCollision(asteroids))
