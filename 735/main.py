177d47066a2fd0199f9adde6c950d03a39270562
Jun <songjeongjun320@gmail.com>
2024-08-12 23:24:45 -0700

    08/12/2024

--- 735/main.py
+++ 735/main.py
@@ -2,17 +2,25 @@ from typing import List, Tuple
 
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        result: List[int] = []
-        if not asteroids:
-            return []
-        
-        result.append(asteroids.pop())
-        for planet in asteroids:
-            tmp: int = asteroids.pop()
-                
-        return
+        result: List[int] = asteroids
+        i: int = 1
+        while 0 < i < len(result):
+            if result[i-1] * result[i] > 0:
+                i += 1
+            else:
+                if abs(result[i-1]) < abs(result[i]):
+                    result.pop(i-1)
+                    i -= 1
+                elif abs(result[i-1]) == abs(result[i]):
+                    result.pop(i-1)
+                    result.pop(i-1)
+                    i -= 2
+                else:
+                    result.pop(i)
+        return result
+
     
 
 sol: Solution = Solution()
-asteroids: List[int] = [5,10,-5]
+asteroids: List[int] = [-8,8]
 print(sol.asteroidCollision(asteroids))
