a8376809c3e7453cc111bd5b18767a2310ba54fa
Jun <songjeongjun320@gmail.com>
2024-08-14 10:28:57 -0700

    08/14/2024

--- 735/main.py
+++ 735/main.py
@@ -4,23 +4,26 @@ class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         result: List[int] = asteroids
         i: int = 1
-        while 0 < i < len(result):
+        while -1 < i < len(result):
             if result[i-1] * result[i] > 0:
                 i += 1
             else:
-                if abs(result[i-1]) < abs(result[i]):
-                    result.pop(i-1)
-                    i -= 1
-                elif abs(result[i-1]) == abs(result[i]):
-                    result.pop(i-1)
-                    result.pop(i-1)
-                    i -= 2
+                if result[i-1] < result[i]:
+                    i += 1
                 else:
-                    result.pop(i)
+                    if abs(result[i-1]) < abs(result[i]):
+                        result.pop(i-1)
+                        i -= 1
+                    elif abs(result[i-1]) == abs(result[i]):
+                        result.pop(i-1)
+                        result.pop(i-1)
+                        i -= 1
+                    else:
+                        result.pop(i)
         return result
 
     
 
 sol: Solution = Solution()
-asteroids: List[int] = [-8,8]
+asteroids: List[int] = [1,-2,-2,1]
 print(sol.asteroidCollision(asteroids))
