4182485b770ba99d2647595f6886c3597d78c54c
Jun <songjeongjun320@gmail.com>
2024-08-16 00:14:43 -0700

    08/16/2024

--- 394/main.py
+++ 394/main.py
@@ -3,7 +3,11 @@ from typing import List, Tuple
 class Solution:
     def decodeString(self, s: str) -> str:
         result: str = ""
+        tmp: str = ""
+        cnt: int = 0
         for i in range(len(s)):
+            result += 
+
             
         return result
     
