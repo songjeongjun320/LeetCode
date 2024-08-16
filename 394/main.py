9b9360078b3c62c1a67b5e5d811c98f8f68fba80
Jun <songjeongjun320@gmail.com>
2024-08-15 17:51:38 -0700

    08/15/2024

---
+++ 394/main.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple
+
+class Solution:
+    def decodeString(self, s: str) -> str:
+        result: str = ""
+        for i in range(len(s)):
+            
+        return result
+    
+
+sol:Solution = Solution()
+s = "3[a2[c]]"
+print(sol.decodeString(s))
