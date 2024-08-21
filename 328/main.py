4e15b9618a22369b5e83f26f74a865cb6d1875d8
Jun <songjeongjun320@gmail.com>
2024-08-20 23:58:26 -0700

    08/20/2024

--- 328/main.py
+++ 328/main.py
@@ -7,4 +7,24 @@ class ListNode:
 
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cnt: int = 0
+        while head:
+            if cnt % 2 == 0:
+                head.next = head.next.next
+            cnt += 1
+            head = head.next
         
+        return head
+    
+
+sol: Solution = Solution()
+
+val:List[int] = [1,2,3,4,5]
+head: ListNode = ListNode(val[0])
+curr:ListNode = head
+for item in val[1:]:
+    curr.next = ListNode(item)
+    curr = curr.next
+
+print(sol.oddEvenList(head))
+
