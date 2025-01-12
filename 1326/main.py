73706d7c15872df1f4f7f2c5097ac0884fc323c4
Jun Song <songjeongjun320@gmail.com>
2025-01-11 23:34:13 -0700

    1326

---
+++ 1326/main.py
@@ -0,0 +1,12 @@
+from typing import List, Tuple
+
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        result:int = 0
+        return result
+    
+
+sol:Solution = Solution()
+n: int = 0
+ranges: List[int] = []
+print(sol.minTaps(n,range))
